260326604f10b5eda051d26c985e4b47b694b268
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-17 20:20:24 +0200

    save changes

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+cd packages/samengine
+bun i
+bun run build
+cd ../..
+
+cd packages/samengine-build
+bun i
+bun run build
+cd ../..
+
+@REM cd packages/samengine-build-react
+@REM bun i
+@REM bun run build
+@REM cd ../..
+
+cd packages/samengine-cli
+bun i
+bun run build
+cd ../..
+
+echo Finished!

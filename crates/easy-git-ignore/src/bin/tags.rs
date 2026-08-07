6f87e4468c2a3ba1c1eec94fc0e14f9aca45c4b9
ShadowDara <shadowdara74@gmail.com>
2026-08-07 12:17:08 +0200

    Delete crates/easy-git-ignore directory

--- crates/easy-git-ignore/src/bin/tags.rs
+++
@@ -1,9 +0,0 @@
-unsafe extern "C" {
-    fn tags_main() -> i32;
-}
-
-fn main() {
-    unsafe {
-        tags_main();
-    }
-}
--- crates/easy-git-ignore/src/bin/unpush.rs
+++
@@ -1,9 +0,0 @@
-unsafe extern "C" {
-    fn u_main() -> i32;
-}
-
-fn main() {
-    unsafe {
-        u_main();
-    }
-}
